cca48930659e7e2a03d4ca9daa0f123358cdfdf3
stefanpante <stefanpante@outlook.com>
2014-06-15 02:12:29 +0200

    Initial layout

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var path = require('path');
+var http = require('http');
 var favicon = require('static-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
@@ -23,7 +24,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 app.use('/users', users);
-
+app.set('port', process.env.PORT || 3000);
 /// catch 404 and forward to error handler
 app.use(function(req, res, next) {
     var err = new Error('Not Found');
@@ -55,5 +56,8 @@ app.use(function(err, req, res, next) {
     });
 });
 
+http.createServer(app).listen(app.get('port'), function(){
+    console.log('Express server listening on port ' + app.get('port'));
+});
 
 module.exports = app;
