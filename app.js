c6785ac93b8e00a6165944287d61058f036ac0ed
JDevlieghere <info@jonasdevlieghere.com>
2014-06-25 14:14:45 +0200

    Minor stuff

--- app.js
+++ app.js
@@ -7,7 +7,6 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
 var routes = require('./routes/index');
-var users = require('./routes/users');
 
 var app = express();
 
@@ -23,8 +22,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
-app.use('/users', users);
-app.set('port', process.env.PORT || 3000);
+
 /// catch 404 and forward to error handler
 app.use(function(req, res, next) {
     var err = new Error('Not Found');
